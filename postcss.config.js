c73f2221efdf4511258e20350207445f9d457275
Utsav Ghimire <utsavghimire@Utsavs-MacBook-Pro.local>
2023-03-14 21:46:21 -0500

    final project code base

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: {
+    tailwindcss:{ content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],},
+    autoprefixer: {},
+  },
+}
